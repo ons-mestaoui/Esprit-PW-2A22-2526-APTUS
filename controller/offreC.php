7add27917ccd54aefe2d86f5ac41874eeef13c22
ons <ons.mestaoui@esprit.tn>
2026-04-19 23:51:18 +0100

    rechercher par titre seulement

--- controller/offreC.php
+++ controller/offreC.php
@@ -85,16 +85,14 @@ class offreC{
             $sql = "SELECT o.*, u.nom as nom_entreprise 
                     FROM offreemploi o 
                     LEFT JOIN utilisateur u ON o.id_entreprise = u.id_utilisateur
-                    WHERE (o.titre LIKE :kw 
-                       OR o.domaine LIKE :kw2
-                       OR u.nom LIKE :kw3)";
+                    WHERE o.titre LIKE :kw";
             if ($onlyActive) {
                 $sql .= " AND o.statut = 'Actif'";
             }
             $sql .= " ORDER BY o.date_publication DESC";
             $like = '%' . $keyword . '%';
             $req = $db->prepare($sql);
-            $req->execute(['kw' => $like, 'kw2' => $like, 'kw3' => $like]);
+            $req->execute(['kw' => $like]);
             return $req;
         } catch (Exception $e) {
             die('Erreur: ' . $e->getMessage());
